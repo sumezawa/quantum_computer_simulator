5f736e92e8196602f12658d6b926cc033d8af76c
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-12-15 18:20:53 -0800

    temporary unitary operator implementation file
    I am still undecided on the hierarchy of this code (operators, gates, circuits, algorithms).

---
+++ src/.h
@@ -0,0 +1,14 @@
+#ifndef OPERATORS_H
+#define OPERATORS_H
+
+#include <vector>
+
+class Operators {
+  public:
+    std::vector<std::vector<unsigned int>> unitary_matrix;
+    
+  private:
+  
+}
+
+#endif
