6e3460854c9ee02bef1ad83e473c9280c6a8bca6
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-12-31 01:25:13 -0800

    Update unitary_operator.cpp

--- src/unitary_operator.cpp
+++ src/unitary_operator.cpp
@@ -1,3 +1,32 @@
-#include "unitary_operator.h"
+#include "unitary.h"
+#include <algorithm> // std::transform
+#include <iostream> // std::ostream
 
-MyClass::MyClass(unsigned int dim) : dimension(dim), unitary_matrix(dim, std::vector<scalar>(dim, 0));
+void unitary_operator::scalar_multiply(scalar s) {
+  for (std::vector<std::vector<scalar>>::iterator rowptr = op.begin(); rowptr != op.end(); ++rowptr) {
+    std::transform(rowptr->begin(), rowptr->end(), rowptr->begin(), [s](int &c){ return c*s; });
+    // std::transform performs an operation on each element of the vector (the row); the parameters specify:
+    // parameter 1, 2 specify the range of elements [ rowptr->begin(), rowptr->end() ) to modify.
+    // parameter 3 specifies the initial iterator where we store the values after modification.
+    // parameter 4 is a lambda function that accepts some scalar 's' as a parameter ('c' is a dummy variable for row elements)
+    // in particular, the function multiplies the row element by 's' and returns the result, for each element of the row.
+  }
+  return;
+}
+
+std::ostream &operator<<(std::ostream &os, unitary_operator op) {
+	for (std::vector<std::vector<scalar>>::iterator rowptr = op.begin(); rowptr != op.end(); ++rowptr) {
+		for (std::vector<scalar>::iterator colptr = rowptr->begin(); colptr != rowptr->end(); ++colptr) {
+			os << *colptr << "\t";
+		}
+		os << std::endl;
+	}
+	return os;
+}
+
+void unitary_operator::print_operator() {
+	std::cout << *this;
+}
+    
+
+ 
