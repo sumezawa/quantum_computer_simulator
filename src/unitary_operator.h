7504b536f032d79837e855e091765a12ce4d0deb
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-12-31 01:25:07 -0800

    Update unitary_operator.h

--- src/unitary_operator.h
+++ src/unitary_operator.h
@@ -1,6 +1,7 @@
 #ifndef UNITARY_OPERATOR_H
 #define UNITARY_OPERATOR_H
 
+#include <cstdlib> // std::size_t
 #include <complex>
 #include <vector>
 
@@ -8,12 +9,16 @@ typedef std::complex<double> scalar;
 
 class unitary_operator {
   public:
-    unitary_operator(unsigned int dimension);
-    unitary_operator scalar_multiply(scalar a);
+    unitary_operator(std::size_t n) : op(n, std::vector<scalar>(n, 0)), dimension(n); // constructor, n x n matrix
+    void scalar_multiply(scalar s);
+  
+    // print
+    friend std::ostream& operator<<(std::ostream& os, unitary_operator& op);
+    void print_operator();
   
   private:
-    std::vector<std::vector<scalar>> matrix;
-    unsigned int dimension;
+    unitary_operator op;
+    std::size_t dimension;
   
 }
 
