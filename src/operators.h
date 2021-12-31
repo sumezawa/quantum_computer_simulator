a80d67da02624a3ee4907bc18721f712e117880e
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-12-31 01:01:03 -0800

    easier filename

--- src/operators.h
+++ src/operators.h
@@ -1,17 +1,18 @@
-#ifndef UNITARY_OPERATOR_H
-#define UNITARY_OPERATOR_H
+#ifndef OPERATORS_H
+#define OPERATORS_H
 
 #include <complex>
 #include <vector>
 
 typedef std::complex<double> scalar;
 
-class unitary_operator {
+class operators {
   public:
     unitary_operator(unsigned int dimension);
-    
+    scalar_multiply(scalar a);
+  
   private:
-    std::vector<std::vector<scalar>> unitary_matrix;
+    std::vector<std::vector<scalar>> matrix;
     unsigned int dimension;
   
 }
